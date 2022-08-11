b45a262f31487825adc6990abe69a157c492c20c
Dima Bart <3088742@mail.ru>
2022-08-12 00:40:14 +0300

    add cart

--- src/components/CartBlock/CartBlock.jsx
+++ src/components/CartBlock/CartBlock.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import './cartBlock.css'
 import {BiCartAlt} from 'react-icons/bi'
+import {CartMenu} from '../CartMenu'
+import { ItemsInCart } from '../ItemsInCart';
+import {calcTotalPrice} from '../utils'
+import { useState } from 'react'
 
 export const CartBlock = () => {
+	const [isCartMenuVisible, setIsCartMenuVisible] = useState(false)
+	const items = useSelector(state => state.cart.itemsInCart)
+	const totalPrice = calcTotalPrice(items)
+
+
   return (
 	<div className='cart-block'>
-		<BiCartAlt size={25} className="cart-block__icon"/>
-		<span className='cart-block__total-price'>2221 руб.</span>
+		<ItemsInCart quantity={items.length}/>
+		<BiCartAlt size={25} className="cart-block__icon" onClick={() => setIsCartMenuVisible(!isCartMenuVisible)}/>
+		{totalPrice > 0 ? <span className='cart-block__total-price'>{totalPrice} руб.</span> : null}
+		{isCartMenuVisible && <CartMenu items={items} onClick={() => null}/>}
 	</div>
   )
 }
--- src/components/GameBuy/GameBuy.jsx
+++ src/components/GameBuy/GameBuy.jsx
@@ -1,15 +1,34 @@
 import React from 'react'
 import './gameBuy.css'
 import { Button } from '../Button'
+import { useDispatch, useSelector } from 'react-redux/'
+import {deleteItemFromCart, setItemInCart} from '../../redux/cart/reducer'
+
 
 
 export const GameBuy = ({game}) => {
+	const dispatch = useDispatch()
+	const items = useSelector(state => state.cart.itemsInCart)
+	const isItemInCart = items.some(item => item.id === game.id)
+
+	const handleClick = (e) => {
+		e.stopPropagation()
+		if(isItemInCart) {
+			dispatch(deleteItemFromCart(game.id))
+		} else{
+
+			dispatch(setItemInCart(game))
+		}
+
+	}
+
+
   return (
-	 <div className='game-buy'>
-		<span className='game-buy__price'>{game.price} руб.</span>
-		<Button onClick={() => {}} type='primary'>
-			В корзину
-		</Button>
-	 </div>
-  )
+    <div className="game-buy">
+      <span className="game-buy__price">{game.price} руб.</span>
+      <Button onClick={handleClick} type={isItemInCart ? "secondary":"primary"}>
+        {isItemInCart ? 'Убрать из корзины' : 'В корзину'}
+      </Button>
+    </div>
+  );
 }
