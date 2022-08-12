f75564bf777bab4f5f83c64ef09efc54d47ef9c5
Dima Bart <3088742@mail.ru>
2022-08-12 12:38:15 +0300

    add cart menu

--- src/components/CartMenu/CartMenu.jsx
+++ src/components/CartMenu/CartMenu.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './cartMenu.css';
 import { calcTotalPrice } from '../utils';
 import { Button } from '../Button';
+import { CartItem } from '../CartItem';
 
 
 
@@ -9,10 +10,12 @@ export const CartMenu = ({ items, onClick }) => {
   return (
     <div className="cart-menu">
       <div className="cart-menu__games-list">
-			{
-			items.length > 0 ? items.map((game) => game.title) : 'Корзина пуста'
-			}
-		</div>
+        {items.length > 0
+          ? items.map((game) => (
+              <CartItem price={game.price} title={game.title} key={game.id} id={game.id} />
+            ))
+          : 'Корзина пуста'}
+      </div>
       {items.length > 0 ? (
         <div className="cart-menu__arrange">
           <div className="cart-menu__total-price">
